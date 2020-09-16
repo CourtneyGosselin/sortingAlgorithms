cff38c69c5b1462d8411eaa50bbe632fef6943e8
Courtney Gosselin <courtney@gosselin.io>
2020-09-15 18:36:44 -0700

    Added first sorting method bubbleSort
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,7 @@
 import React, { Component, useEffect, useState} from "react";
 import * as d3 from "d3";
 import BarChart from "./barChart";
+import { timeout } from "d3";
 
 
 class Main extends Component {
@@ -9,51 +10,33 @@ class Main extends Component {
     this.state = {
       title: "Unsorted Data",
       dataset: [12, 31, 22, 17, 25, 18, 29, 14, 9, 3],
-      svg: null,
     };
   }
 
-  newBarChart = () => {
-    const {dataset, svg} = this.state;
-    const w = 600;
-    const h = 400;
-
-    const svgNew = d3
-    .select(this.refs.chart)
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h)
-    .attr("class", "bar")
-    svgNew
-    .selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("fill", "#00FFB2")
-    .attr("class", "sBar")
-    .attr("x", (d, i) => i * 60)
-    .attr("y", (d, i) => {
-       return h - 7 * d;
-     })
-    .attr("width", 40)
-    .attr("height", (d, i) => 7 * d)
-    .append("title")
-    .text(d => d);
-  svgNew
-    .selectAll("text")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .style("font-size", 18)
-    .attr("fill", "#FFFFFF")
-    .attr("x", (d, i) => i * 60)
-    .attr("y", (d, i) => h - 7 * d - 3)
-    .text(d => d);
-
-    this.setState({
-      svg: svgNew,
-    });
+  
+  bubbleSort = () => {
+    const {dataset, sortSteps} = this.state;
+    const tempDataSet = [];
+    for(i = 0; i<dataset.length; i++){
+      tempDataSet[i] = dataset[i];
+    }
+    console.log("Data before: " + dataset);
+    for(var i = 0; i < dataset.length-1; i++){
+        for(var j = 0; j < dataset.length - i - 1; j++){
+            if(tempDataSet[j]> tempDataSet[j+1]){
+               var temp = tempDataSet[j];
+               tempDataSet[j] = tempDataSet[j+1];
+               tempDataSet[j+1]= temp;
+               this.setState({
+                dataset: tempDataSet,
+              },)
+            }
+        }
+    }
+   console.log("Bubble sort finished: " + dataset);
   }
+  
+  
 
   randomArray = () => {
     const {dataset} = this.state;
@@ -71,20 +54,16 @@ class Main extends Component {
 
   render() {
     const {title, dataset} = this.state;
-    
 
-
-    
-  
     return (
-      <div className="main">
+      <div className="main d-flex justify-content-center">
         <div  ref="chart" className="float-left" >
           <h1 style={{ textAlign: "center" }}>{title}</h1>
           <BarChart width={600} height={400} data={dataset} />
         </div> 
         <div>
           <button type="button" className="btn button-primary float-right text-black" onClick={this.randomArray}>New Data</button>
-          <button type="button" className="btn button-primary float-right text-black">Insertion Sort</button>
+          <button type="button" className="btn button-primary float-right text-black" onClick={()=>this.bubbleSort()}>Bubble Sort</button>
         </div> 
       </div>
     );
